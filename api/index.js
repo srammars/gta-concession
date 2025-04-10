3ef31fa9463adc8fa49badcef42071284d29e71f
srammars <sujeenthiran333@gmail.com>
2025-04-10 12:55:20 +0200

    Initial commit

--- api/index.js
+++ api/index.js
@@ -4,23 +4,19 @@ const multer = require('multer');
 const cloudinary = require('cloudinary').v2;
 
 const app = express();
-const port = 3000;
+const port = 3000;  // Port par défaut pour développement, mais Vercel choisira automatiquement un port.
 
-// Configurer Cloudinary avec les variables d'environnement
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
 });
 
-// Middleware pour gérer les fichiers téléchargés
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Permet de gérer les requêtes JSON et les fichiers
 app.use(express.json());
 
-// Endpoint pour uploader une image sur Cloudinary
 app.post('/upload', upload.fields([
   { name: 'image' }, 
   { name: 'permis' }, 
@@ -33,9 +29,8 @@ app.post('/upload', upload.fields([
   const files = req.files;
   const promises = [];
 
-  // Upload de chaque fichier vers Cloudinary
   Object.keys(files).forEach(fileKey => {
-    const file = files[fileKey][0];  // Il peut y avoir plusieurs fichiers par clé
+    const file = files[fileKey][0];
     const uploadPromise = new Promise((resolve, reject) => {
       cloudinary.uploader.upload_stream({ resource_type: 'auto' }, (error, result) => {
         if (error) reject(error);
@@ -45,13 +40,12 @@ app.post('/upload', upload.fields([
     promises.push(uploadPromise);
   });
 
-  // Attendre que tous les fichiers soient uploadés
   Promise.all(promises)
     .then(urls => {
       res.json({
-        imageUrl: urls[0],   // URL de l'image principale
-        permisUrl: urls[1],  // URL du permis de conduire
-        identiteUrl: urls[2] // URL de la pièce d'identité
+        imageUrl: urls[0],
+        permisUrl: urls[1],
+        identiteUrl: urls[2]
       });
     })
     .catch(error => {
@@ -59,7 +53,6 @@ app.post('/upload', upload.fields([
     });
 });
 
-// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Serveur backend démarré sur http://localhost:${port}`);
 });
